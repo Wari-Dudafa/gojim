b4e6645fba029792ee4e2614ea8fd3d01ae014ed
Wari <60980271+Wari-Dudafa@users.noreply.github.com>
2023-07-13 15:45:18 +0100

    👉🏾 exercise_swiping

--- app/components/NewExerciseSelector.js
+++ app/components/NewExerciseSelector.js
@@ -1,26 +1,99 @@
-import { View, Text, StyleSheet, Image, ScrollView } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  ScrollView,
+  TouchableOpacity,
+  Animated,
+  Alert,
+} from "react-native";
+import Swipeable from "react-native-gesture-handler/Swipeable";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 function NewExerciseSelector(props) {
   // For each exercise in the array, an exercise selector component is rendered
 
   return (
-    <ScrollView style={styles.scroll}>
-      {props.exercises.map((item, index) => {
-        return (
-          <View style={styles.container} key={index}>
-            <View key={index} style={styles.exerciseContainer}>
-              <Text style={styles.name}>{item.name}</Text>
-              <Text style={styles.reps}>{item.reps} reps</Text>
-              <Text style={styles.sets}>{item.sets} sets </Text>
-              <Image
-                style={styles.image}
-                source={require("../../assets/shading.png")}
-              />
-            </View>
-          </View>
-        );
-      })}
-    </ScrollView>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <ScrollView style={styles.scroll}>
+        {props.exercises.map((item, index) => {
+          RenderRightActions = (progress) => {
+            RenderRightAction = (text, color, x, progress, onPress) => {
+              const trans = progress.interpolate({
+                inputRange: [0, 1],
+                outputRange: [x, 0],
+              });
+
+              return (
+                <Animated.View
+                  style={{
+                    flex: 1,
+                    transform: [{ translateX: 0 }],
+                    marginVertical: 10,
+                  }}
+                >
+                  <TouchableOpacity
+                    style={[styles.rightAction, { backgroundColor: color }]}
+                    onPress={onPress}
+                  >
+                    <Text style={styles.rightActionText}>{text}</Text>
+                  </TouchableOpacity>
+                </Animated.View>
+              );
+            };
+
+            return (
+              <View style={{ width: 192, flexDirection: "row-reverse" }}>
+                {RenderRightAction("Delete", "red", 128, progress, Delete)}
+                {RenderRightAction("Edit", "blue", 192, progress, Edit)}
+              </View>
+            );
+          };
+
+          const Edit = () => {
+            // Edit excercise object
+            Alert.alert("Edited");
+          };
+
+          const Delete = () => {
+            // Delete excercise object from array
+            // Set updated array ad main array
+            Alert.alert("Deleted");
+            Close();
+          };
+
+          UpdateRef = (ref) => {
+            swipeableRow = ref;
+          };
+
+          Close = () => {
+            swipeableRow.close();
+          };
+
+          return (
+            <Swipeable
+              ref={UpdateRef}
+              friction={2}
+              rightThreshold={50}
+              renderRightActions={RenderRightActions}
+            >
+              <View style={styles.container} key={index}>
+                <View key={index} style={styles.exerciseContainer}>
+                  <Text style={styles.name}>{item.name}</Text>
+                  <Text style={styles.reps}>{item.reps} reps</Text>
+                  <Text style={styles.sets}>{item.sets} sets </Text>
+                  <Image
+                    style={styles.image}
+                    source={require("../../assets/shading.png")}
+                  />
+                </View>
+              </View>
+            </Swipeable>
+          );
+        })}
+      </ScrollView>
+    </GestureHandlerRootView>
   );
 }
 
@@ -66,5 +139,17 @@ const styles = StyleSheet.create({
   scroll: {
     flex: 1,
     paddingBottom: 20,
-  }
+  },
+  rightAction: {
+    borderRadius: 10,
+    alignItems: "center",
+    flex: 1,
+    justifyContent: "center",
+    marginRight: 10,
+  },
+  rightActionText: {
+    color: "#e6e6e6",
+    fontSize: 20,
+    padding: 10,
+  },
 });
