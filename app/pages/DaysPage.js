1b7f7d1bd3bf420a8290dd451acab5e0ac57d272
Wari <60980271+Wari-Dudafa@users.noreply.github.com>
2023-09-14 19:05:27 +0100

    🔥 Weekly gym streak

--- app/pages/DaysPage.js
+++ app/pages/DaysPage.js
@@ -27,23 +27,82 @@ function DaysPage(props) {
     });
   };
 
-  const getWeeklyStreak = () => {
-    let streak = 5000;
-    let secondaryMessage;
+  const areDatesInSameWeek = (date1, date2) => {
+    // Get the year and week number for the first date
+    let year1 = date1.getFullYear();
+    let week1 = getWeekNumber(date1);
 
-    // Calculate the weekly streak
+    // Get the year and week number for the second date
+    let year2 = date2.getFullYear();
+    let week2 = getWeekNumber(date2);
 
-    if (streak == 0) {
-      secondaryMessage = "Only up from here!";
-    } else if (streak == 1) {
-      secondaryMessage = "Starting is the first step!";
-    } else {
-      secondaryMessage = "Keep up the good work!";
+    // Check if the years and week numbers are the same
+    return year1 === year2 && week1 === week2;
+  };
+
+  const getWeekNumber = (date) => {
+    let target = new Date(date.valueOf());
+    let dayNr = (date.getDay() + 6) % 7;
+    target.setDate(target.getDate() - dayNr + 3);
+    let firstThursday = target.valueOf();
+    target.setMonth(0, 1);
+    
+    if (target.getDay() !== 4) {
+      target.setMonth(0, 1 + ((4 - target.getDay() + 7) % 7));
     }
+    return 1 + Math.ceil((firstThursday - target) / 604800000); // 604800000 = 7 * 24 * 3600 * 1000
+  };
 
-    Alert.alert(
-      "Your weekly gym streak is: " + streak + " " + secondaryMessage
-    );
+  const getWeeklyStreak = () => {
+    let statement = "SELECT * FROM session";
+    db.sql(statement, (resultSet) => {
+      let streak = 0;
+      let shouldCalculateStreak = true;
+      let secondaryMessage;
+      let lastSession;
+      let sessions = resultSet.rows._array.reverse();
+
+      // Calculate the weekly streak
+      if (sessions.length == 0) {
+        // Do nothing
+      } else {
+        for (let index = 0; index < sessions.length; index++) {
+          let session = new Date(sessions[index].date);
+          console.log(String(session));
+
+          if (index == 0) {
+            lastSession = new Date("04/09/2004");
+          } else {
+            lastSession = new Date(sessions[index - 1].date);
+          }
+
+          if (
+            areDatesInSameWeek(lastSession, session) &&
+            shouldCalculateStreak
+          ) {
+            if (lastSession.getDate() == session.getDate()) {
+              // Do nothing
+            } else {
+              streak++;
+            }
+          } else {
+            shouldCalculateStreak = false;
+          }
+        }
+      }
+
+      if (streak == 0) {
+        secondaryMessage = "Only up from here!";
+      } else if (streak == 1) {
+        secondaryMessage = "Starting is the first step!";
+      } else {
+        secondaryMessage = "Keep up the good work!";
+      }
+
+      Alert.alert(
+        "Your weekly gym streak is: " + streak + " " + secondaryMessage
+      );
+    });
   };
 
   return (
