668e19e4f52cfcd6101f8ae9c4a04158db31f37a
Wari <60980271+Wari-Dudafa@users.noreply.github.com>
2023-10-01 12:42:58 +0100

    🗑️ Deleting data works now

--- app/pages/SettingsPage.js
+++ app/pages/SettingsPage.js
@@ -47,35 +47,7 @@ function SettingsPage(props) {
     }
   };
 
-  const deleteData = () => {
-    Alert.alert(
-      "Confirmation",
-      "Are you sure you want to delete all your data?",
-      [
-        { text: "No", style: "cancel" },
-        {
-          text: "Yes",
-          style: "destructive",
-          onPress: async () => {
-            db.wipeDatabase();
-            let keyToDelete = "firstTimeOpening";
-            try {
-              await AsyncStorage.removeItem(keyToDelete);
-              Alert.alert("Confirmation", "Data deleted successfully");
-              setFirstTimeOpening(false);
-            } catch (error) {
-              console.error("Error deleting key", error);
-            }
-          },
-        },
-      ],
-      { cancelable: false }
-    );
-  };
-
-  const setFirstTimeOpening = async (shouldAskConfirmation) => {
-    let keyToDelete = "firstTimeOpening";
-
+  const setFirstTimeOpening = (shouldAskConfirmation) => {
     if (shouldAskConfirmation) {
       Alert.alert(
         "Confirmation",
@@ -94,6 +66,126 @@ function SettingsPage(props) {
       );
     } else {
       props.navigation.navigate("SetupStack");
+    }
+  };
+
+  const deleteAllData = async (shouldAskConfirmation) => {
+    if (shouldAskConfirmation) {
+      Alert.alert(
+        "Confirmation",
+        "Are you sure you want to delete all your data?",
+        [
+          { text: "No", style: "cancel" },
+          {
+            text: "Yes",
+            style: "destructive",
+            onPress: async () => {
+              db.wipeDatabase();
+              let keyToDelete = "firstTimeOpening";
+              try {
+                await AsyncStorage.removeItem(keyToDelete);
+                Alert.alert("Confirmation", "Data deleted successfully");
+                setFirstTimeOpening(false);
+              } catch (error) {
+                console.error("Error deleting key", error);
+              }
+            },
+          },
+        ],
+        { cancelable: false }
+      );
+    } else {
+      db.wipeDatabase();
+      let keyToDelete = "firstTimeOpening";
+      try {
+        await AsyncStorage.removeItem(keyToDelete);
+        Alert.alert("Confirmation", "Data deleted successfully");
+        setFirstTimeOpening(false);
+      } catch (error) {
+        console.error("Error deleting key", error);
+      }
+    }
+  };
+
+  const deleteDaysData = (shouldAskConfirmation) => {
+    if (shouldAskConfirmation) {
+      Alert.alert(
+        "Confirmation",
+        "Are you sure you want to delete all the days data?",
+        [
+          { text: "No", style: "cancel" },
+          {
+            text: "Yes",
+            style: "destructive",
+            onPress: () => {
+              db.dropTable("days");
+              db.dropTable("weight_per_set");
+              db.dropTable("reps_per_set");
+              db.dropTable("sets");
+              db.dropTable("session");
+              db.dropTable("exercises");
+              db.init();
+            },
+          },
+        ],
+        { cancelable: false }
+      );
+    } else {
+      db.dropTable("days");
+      db.dropTable("weight_per_set");
+      db.dropTable("reps_per_set");
+      db.dropTable("sets");
+      db.dropTable("session");
+      db.dropTable("exercises");
+      db.init();
+    }
+  };
+
+  const deleteFoodData = (shouldAskConfirmation) => {
+    if (shouldAskConfirmation) {
+      Alert.alert(
+        "Confirmation",
+        "Are you sure you want to delete all the food data?",
+        [
+          { text: "No", style: "cancel" },
+          {
+            text: "Yes",
+            style: "destructive",
+            onPress: () => {
+              db.dropTable("meals");
+              db.init();
+            },
+          },
+        ],
+        { cancelable: false }
+      );
+    } else {
+      db.dropTable("meals");
+      db.init();
+    }
+  };
+
+  const deleteScaleData = (shouldAskConfirmation) => {
+    if (shouldAskConfirmation) {
+      Alert.alert(
+        "Confirmation",
+        "Are you sure you want to delete all the scale data?",
+        [
+          { text: "No", style: "cancel" },
+          {
+            text: "Yes",
+            style: "destructive",
+            onPress: () => {
+              db.dropTable("user_weight");
+              db.init();
+            },
+          },
+        ],
+        { cancelable: false }
+      );
+    } else {
+      db.dropTable("user_weight");
+      db.init();
     }
   };
 
@@ -123,26 +215,13 @@ function SettingsPage(props) {
           title="Back to setup"
           onPress={() => setFirstTimeOpening(true)}
         />
-        <Button
-          title="Delete days data"
-          onPress={() => {
-            db.dropTable("days");
-            db.dropTable("weight_per_set");
-            db.dropTable("reps_per_set");
-            db.dropTable("sets");
-            db.dropTable("session");
-            db.dropTable("exercises");
-          }}
-        />
-        <Button
-          title="Delete food data"
-          onPress={() => db.dropTable("meals")}
-        />
+        <Button title="Delete days data" onPress={() => deleteDaysData(true)} />
+        <Button title="Delete food data" onPress={() => deleteFoodData(true)} />
         <Button
           title="Delete scale data"
-          onPress={() => db.dropTable("user_weight")}
+          onPress={() => deleteScaleData(true)}
         />
-        <Button title="Delete all data" onPress={deleteData} />
+        <Button title="Delete all data" onPress={() => deleteAllData(true)} />
 
         <Text style={[styles.creditText, { color: theme.colors.onBackground }]}>
           {Application.applicationName} version{" "}
