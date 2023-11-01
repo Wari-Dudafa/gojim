237c0f927a7b767ce627309c85aac4a174b82da9
Wari <60980271+Wari-Dudafa@users.noreply.github.com>
2023-11-01 20:20:08 +0000

    2️⃣ Gojim version 2

--- App.js
+++ App.js
@@ -1,88 +1,9 @@
-import { useEffect, useState } from "react";
-import { PaperProvider } from "react-native-paper";
-import { useColorScheme } from "react-native";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { preventAutoHideAsync, hideAsync } from "expo-splash-screen";
-
-import AppStack from "./app/navigation/AppStack";
-import LightTheme from "./app/utils/LightTheme";
-import DarkTheme from "./app/utils/DarkTheme";
-import Database from "./app/classes/DatabaseClass";
-import SplashScreen from "./app/utils/SplashScreen";
-
-// Keep the splash screen visible
-preventAutoHideAsync();
+import { View, Text } from "react-native";
 
 export default function App() {
-  const db = new Database();
-  const colorScheme = useColorScheme();
-  const [theme, setTheme] = useState(DarkTheme);
-  const [firstTimeOpening, setFirstTimeOpening] = useState(true);
-  const [splashScreen, setSplashScreen] = useState(true);
-  const [appIsReady, setAppIsReady] = useState(false);
-
-  useEffect(() => {
-    checkFirstTimeOpening();
-  }, []);
-
-  useEffect(() => {
-    determineColorScheme();
-  }, [colorScheme]);
-
-  const checkFirstTimeOpening = async () => {
-    try {
-      let storedFirstTimeOpening = await AsyncStorage.getItem(
-        "firstTimeOpening"
-      );
-      if (storedFirstTimeOpening === null) {
-        // This is the first time the app is being opened
-        db.init();
-        setFirstTimeOpening(true);
-      } else {
-        setFirstTimeOpening(false);
-      }
-    } catch (error) {
-      console.error("Error checking first-time opening:", error);
-    } finally {
-      // Tell the application to render
-      setAppIsReady(true);
-      hideAsync(); // This causes a momentary flash of white as the app mounts
-    }
-  };
-
-  const determineColorScheme = () => {
-    if (colorScheme === "dark") {
-      // Render dark mode
-      setTheme(DarkTheme);
-    } else {
-      // Render light mode
-      setTheme(LightTheme);
-    }
-  };
-
-  if (!appIsReady) {
-    return null;
-  }
-
-  if (splashScreen) {
-    return (
-      <PaperProvider theme={theme}>
-        <SplashScreen setSplashScreen={setSplashScreen} />
-      </PaperProvider>
-    );
-  }
-
-  if (firstTimeOpening) {
-    return (
-      <PaperProvider theme={theme}>
-        <AppStack initialRouteName="SetupStack" />
-      </PaperProvider>
-    );
-  }
-
   return (
-    <PaperProvider theme={theme}>
-      <AppStack initialRouteName="TabBarStack" />
-    </PaperProvider>
+    <View style={{ flex: 1 }}>
+      <Text>Gojim 2.0</Text>
+    </View>
   );
 }
--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # Gojim Fitness App
 
-<img src="./assets/icon.png" alt="Gojim Logo"  width="350">
-
 Gojim is a comprehensive fitness app designed to help you track your workouts and calories, allowing you to stay motivated and achieve your fitness goals. With Gojim, you can easily monitor your progress, record your exercises, and keep an eye on your calorie intake.
 
 ## Features
@@ -10,14 +8,6 @@ Gojim is a comprehensive fitness app designed to help you track your workouts an
 
 - **Calorie Counter**: Achieving your fitness goals requires more than just exercise; it's also about nutrition. With Gojim's integrated calorie counter, you can easily monitor your daily calorie intake. Our extensive database, powered by [Open Food Facts](https://openfoodfacts.org), simplifies calorie tracking by providing nutritional information for a wide range of common food items. Say goodbye to guesswork and hello to precision in your diet planning.
 
-## Screenshots
-
-<div style="display: flex; flex-direction: row;">
-  <img style="margin-right: 10px" src="./screenshots/DaysPage1.png" alt="DaysPage" width="200">
-  <img style="margin-left: 10px; margin-right: 10px"  src="./screenshots/FoodPage1.png" alt="FoodPage" width="200">
-  <img style="margin-left: 10px; margin-right: 10px"  src="./screenshots/ScalePage1.png" alt="ScalePage" width="200">
-  <img style="margin-left: 10px" src="./screenshots/WeightLog1.png" alt="WeightLog" width="200">
-</div>
 
 ## Technologies Used
 
@@ -31,7 +21,7 @@ Contributions to Gojim are always welcome! If you have any ideas, improvements, 
 
 ## Download
 
-The app will be available to download on ios shortly, the android build is currently in the works.
+The app is be available to download on ios, the android build is currently in the works.
 
 ios: [download link](https://apps.apple.com/app/id6467132061)
 android: [download link](https://github.com/Wari-Dudafa/gojim)
